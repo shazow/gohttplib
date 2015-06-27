637a6b3a8541948606bf2ee983637827e385c4d1
Andrey Petrov <andrey.petrov@shazow.net>
2015-06-27 18:11:18 +0200

    More sensible Python wrapper.

--- examples/python/README.rst
+++ examples/python/README.rst
@@ -1,4 +1,26 @@
 gohttp: Bindings for gohttplib
 ==============================
 
+*Warning*: This library currently ships with a shared object of gohttplib
+compiled for OSX. It will not work on other platforms at the moment.
+
 See `https://github.com/shazow/gohttplib`_ for details.
+
+
+Usage
+-----
+
+    from gohttp import route, run
+
+    @route('/')
+    def index(w, req):
+        w.write("%s %s %s\n" % (req.method, req.host, req.url))
+        w.write("Hello, world.\n")
+
+    run(host='127.0.0.1', port=5000)
+
+
+License
+-------
+
+MIT.
--- examples/python/gohttp/__init__.py
+++ examples/python/gohttp/__init__.py
@@ -3,15 +3,69 @@ from ._gohttplib import ffi
 
 lib = ffi.dlopen(os.path.join(os.path.dirname(__file__), "libgohttp.so"))
 
+# Hang onto handlers so they don't get gc'd
+_handlers = []
 
-@ffi.callback("void(ResponseWriter*, Request*)")
-def handler(w, req):
-    body = "Hello, world"
-    n = lib.ResponseWriter_Write(w, body, len(body))
 
-    if n < 0:
-        print("handler: Failed to write.\n")
-        lib.ResponseWriter_WriteHeader(w, 500)
-        return
+class ResponseWriter:
+    def __init__(self, w):
+        self._w = w
 
-    print("handler: Wrote %d bytes." % n)
+    def write(self, body):
+        n = lib.ResponseWriter_Write(self._w, body, len(body))
+        if n != len(body):
+            raise IOError("Failed to write to ResponseWriter.")
+
+    def set_status(self, code):
+        lib.ResponseWriter_WriteHeader(self._w, code)
+
+
+class Request:
+    def __init__(self, req):
+        self._req = req
+
+    @property
+    def method(self):
+        return ffi.string(self._req.Method)
+
+    @property
+    def host(self):
+        return ffi.string(self._req.Host)
+
+    @property
+    def url(self):
+        return ffi.string(self._req.URL)
+
+    def __repr__(self):
+        return "{self.method} {self.url}".format(self=self)
+
+
+def route(pattern, fn=None):
+    """
+    Can be used as a decorator.
+
+    :param pattern:
+        Address pattern to match against.
+
+    :param fn:
+        Handler to call when pattern is matched. Handler is given a
+        ResponseWriter and Request object.
+    """
+    def wrapped(fn):
+        @ffi.callback("void(ResponseWriter*, Request*)")
+        def handler(w, req):
+            fn(ResponseWriter(w), Request(req))
+
+        lib.HandleFunc(pattern, handler)
+        _handlers.append(handler)
+
+    if fn:
+        return wrapped(fn)
+
+    return wrapped
+
+
+def run(host='127.0.0.1', port=5000):
+    bind = '{}:{}'.format(host or '', port)
+    print(" * Running on http://{}/".format(bind))
+    lib.ListenAndServe(bind)
